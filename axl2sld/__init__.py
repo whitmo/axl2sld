6e9d2bdebf7eabd0f94cbaf488855f6c92f0188a
whit <whit@openplans.org>
2009-04-08 09:00:27 -0500

    basic folder and file creation

--- axl2sld/__init__.py
+++ axl2sld/__init__.py
@@ -1,14 +1,121 @@
-#
+from copy import copy
 from lxml import etree
+from lxml.builder import ElementMaker
 from lxml.etree import tostring
+from pprint import pprint
+from utils import arg_parser
+import itertools
 import optparse
 import os
-from utils import arg_parser
-from lxml.builder import ElementMaker
-from copy import copy
-from pprint import pprint
-import itertools
+import string
+
 curdir = os.path.abspath(os.curdir)
+
+nsmap = dict(sld="http://www.opengis.net/sld",
+             ogc="http://www.opengis.net/ogc",
+             xlink="http://www.w3.org/1999/xlink",
+             xsi="http://www.w3.org/2001/XMLSchema-instance")
+
+
+def emaker(prefix):
+    nsmap2 = copy(nsmap)
+    ns = nsmap2.pop(prefix)
+    return ElementMaker(namespace=ns, nsmap=nsmap2)
+
+
+SLD = emaker('sld')
+
+def sld_subelement(parent, name):
+    ele = SLD(name)
+    parent.append(ele)
+    return ele
+
+
+def build_sld_trees(axls):
+    for axl in axls:
+        axltree = etree.parse(axl)
+        for layer in axltree.xpath(".//LAYER"):
+            yield transform_to_sldtree(layer)
+
+
+def transform_to_sldtree(layer):
+    sldtree = SLD('StyledLayerDescriptor',
+                  {"schemaLocation":"http://www.opengis.net/sld StyledLayerDescriptor.xsd",
+                   "version":'1.0.0'})
+    namedlayer = sld_subelement(sldtree, 'NamedLayer')
+    name = sld_subelement(namedlayer, "Name")
+    layer_name = name.text = "alachua:%s" %layer.attrib['id'].split('.')[1]
+
+    userstyle = sld_subelement(namedlayer, "UserStyle")
+    fts = sld_subelement(userstyle, "FeatureTypeStyle")
+    sld_subelement(fts, "Name").text = layer_name
+    sld_subelement(fts, "Title").text = layer.attrib['name']
+    sld_subelement(fts, "Abstract").text = "styles for the alachua project"
+    sld_subelement(fts, "FeatureTypeName").text = layer_name
+    add_rules(fts, layer)
+    return sldtree
+    
+
+rule_map = dict(SHIELDSYMBOL=None,
+                RASTERMARKERSYMBOL=None,
+                SIMPLEPOLYGONSYMBOL='PolygonSymbolizer',
+                SIMPLELINESYMBOL='LineSymbolizer',
+                TEXTSYMBOL='TextSymbolizer',
+                TRUETYPEMARKERSYMBOL='TextSymbolizer',
+                SIMPLEMARKERSYMBOL='PointSymbolizer')
+
+
+def add_rules(ele, layer):
+    "add rules to fts 'ele'"
+    gr = layer.xpath("./GROUPRENDERER")
+    if not len(gr):
+        return
+    gr = gr.pop()
+
+    symbols = [gr.xpath('.//%s' %tag) for tag in rule_map.keys() if rule_map[tag]]
+
+    for sym in itertools.chain(*symbols):
+        # tags.update((x, None) for x in sym.attrib.keys()) # debug
+        tag = sym.getparent().tag
+        rule_handler.get(tag, normal_rule)(sym, ele, layer)
+
+def filter_rule(ele, symbol, layer):
+    "build rule with filter ala RANGE"
+    rule = normal_rule(ele, symbol)
+    return rule
+
+def normal_rule(ele, symbol, layer=None):
+    "nothing fancy rule"
+    rule = sld_subelement(ele, "Rule")
+    parent = symbol.getparent()
+    title = parent.attrib.get('label', None)
+    if title is not None:
+        sld_subelement(rule, "Title").text = title
+    sld_sym = sld_subelement(rule, rule_map[ele.tag])
+    
+    return rule
+
+
+
+def convert_to_hex_letter(num):
+    dl = string.hexdigits
+    dn = [16*x for x in range(len(string.hexdigits))]
+    for val in dn:
+        if num == val:
+            return "%s0" %dl[num/16]
+        if num > val:
+            last = val
+            continue
+        else:
+            return "%s%s" %(dl[last], dl[num - last])
+        
+
+
+rule_handler = dict(RANGE=filter_rule,
+                    EXACT=normal_rule,
+                    OTHER=normal_rule)
+
+# main script
 
 usage = "usage: %prog [options] inputdir [inputdir, etc]"
 main_parser = optparse.OptionParser(usage=usage)
@@ -24,9 +131,22 @@ def main(args=None, options=None, parser=None):
     listdir = os.listdir(args[1]) # multiples?
     axls = [x for x in listdir if x.endswith('.axl')]
     sld_trees = build_sld_trees(axls)
+    if not os.path.exists(options.output_dir):
+        os.mkdir(options.output_dir)
+    findname = etree.ETXPath("//{http://www.opengis.net/sld}NamedLayer/{http://www.opengis.net/sld}Name")
     for tree in sld_trees:
-        continue
-    pprint(tags.keys())
+        assert len(tree)
+        res = findname(tree)
+        assert len(res), ValueError(tostring(tree, pretty_print=True))
+        dummy, name = res[0].text.split(":")
+        print name
+        fh = open(os.path.join(options.output_dir, name), 'w')
+
+
+    #pprint(tags.keys())
+
+
+# explorer script
 
 usage = "usage: %prog [options] inputdir"
 explorer_parser = optparse.OptionParser(usage=usage)
@@ -43,8 +163,9 @@ explorer_parser.add_option('-e', '--extension',
                            dest="file_ext",
                            default='.sld')
 
-
+# debugging globals for data exploration
 ATTRIBS = dict()
+tags = dict()
 
 @arg_parser(explorer_parser)
 def explorer(args=None, options=None, parser=None):
@@ -61,91 +182,6 @@ def explorer(args=None, options=None, parser=None):
     pprint(ATTRIBS.keys())
 
 
-def build_sld_trees(axls):
-    for axl in axls:
-        axltree = etree.parse(axl)
-        for layer in axltree.xpath(".//LAYER"):
-            yield transform_to_sldtree(layer)
-
-def transform_to_sldtree(layer):
-    sldtree = SLD('StyledLayerDescriptor',
-                  {"schemaLocation":"http://www.opengis.net/sld StyledLayerDescriptor.xsd",
-                   "version":'1.0.0'})
-    namedlayer = SLD.subElement(sldtree, 'NamedLayer')
-    name = SLD.subElement(namedlayer, "Name")
-    layer_name = name.text = "alachua:%s" %layer.attrib['id'].split('.')[1]
-
-    userstyle = SLD.subElement(namedlayer, "UserStyle")
-    fts = SLD.subElement(userstyle, "FeatureTypeStyle")
-    SLD.subElement(fts, "Name").text = layer_name
-    SLD.subElement(fts, "Title").text = layer.attrib['name']
-    SLD.subElement(fts, "Abstract").text = "styles for the alachua project"
-    SLD.subElement(fts, "FeatureTypeName").text = layer_name
-    add_rules(fts, layer)
-    return sldtree
-    
-
-rule_map = dict(SHIELDSYMBOL=None,
-                RASTERMARKERSYMBOL=None,
-                SIMPLEPOLYGONSYMBOL='PolygonSymbolizer',
-                SIMPLELINESYMBOL='LineSymbolizer',
-                TEXTSYMBOL='TextSymbolizer',
-                TRUETYPEMARKERSYMBOL='TextSymbolizer',
-                SIMPLEMARKERSYMBOL='PointSymbolizer')
-
-tags = dict()
-
-
-def add_rules(ele, layer):
-    gr = layer.xpath("./GROUPRENDERER")
-    if not len(gr):
-        return
-    gr = gr.pop()
-
-
-    symbols = [gr.xpath('.//%s' %tag) for tag in rule_map.keys() if rule_map[tag]]
-
-    for sym in itertools.chain(*symbols):
-        tags.update((x, None) for x in sym.attrib.keys())
-#         tag = sym.getparent().tag
-#         #rule_handler.get(tag, normal_rule)(sym, ele)
-
-def filter_rule(ele, symbol):
-    "build rule with filter"
-    rule = normal_rule(ele, symbol)
-    return rule
-
-def normal_rule(ele, symbol):
-    "nothing fancy rule"
-    rule = SLD.subElement(ele, "Rule")
-    parent = symbol.getparent()
-    title = parent.attrib.get('label', None)
-    if title is not None:
-        SLD.subElement(rule, "Title").text = title
-    sld_sym = SLD.subElement(rule, rule_map[symbol.tag])
-    
-    return rule
-
-import string
-
-def convert_to_hex_letter(num):
-    dl = string.hexdigits
-    dn = [16*x for x in range(len(string.hexdigits))]
-    for val in dn:
-        if num == val:
-            return "%s0" %dl[num/16]
-        if num > val:
-            last = val
-            continue
-        else:
-            return "%s%s" %(dl[last], dl[num - last])
-        
-
-rule_handler = dict(RANGE=filter_rule,
-                    EXACT=normal_rule,
-                    OTHER=normal_rule)
-
-css_param_map = dict()
 
 # we need to create a mapping between attributes in axl symbols
 # and resulting CssParameter names
@@ -210,19 +246,3 @@ css_param = [
     'stroke-opacity',
     'stroke-width',#
     ]
-
-
-
-
-nsmap = dict(sld="http://www.opengis.net/sld",
-             ogc="http://www.opengis.net/ogc",
-             xlink="http://www.w3.org/1999/xlink",
-             xsi="http://www.w3.org/2001/XMLSchema-instance")
-
-def emaker(prefix):
-    return ElementMaker(namespace=nsmap[prefix], nsmap={prefix:nsmap[prefix]})
-
-
-
-SLD = emaker('sld')
-
